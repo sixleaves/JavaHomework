e92a0696f51631e83a4a94b5e7ac0b74ed1c5ced
sweetcs <sweetcs@sweetcss-MacBook-Pro.local>
2018-07-20 19:36:30 +0800

    day04 homework

--- chapter1/src/main/java/day04/homework/Question7.java
+++ chapter1/src/main/java/day04/homework/Question7.java
@@ -14,25 +14,25 @@ public class Question7 {
     public static void main(String[] args) {
 
         int totalLines = 5;
-        int numStart = totalLines - 2;
+        int numStar = totalLines - 2;
 
         for (int i = 0; i < 5; i++) {
 
             if (i > totalLines / 2) {
 
-                numStart = totalLines - 2*(i - totalLines / 2);
-                int numLeftBlack = (totalLines - numStart) / 2;
+                numStar = totalLines - 2*(i - totalLines / 2);
+                int numLeftBlack = (totalLines - numStar) / 2;
                 printBlank(numLeftBlack);
-                for (int j = 0; j <numStart;j++)
+                for (int j = 0; j <numStar;j++)
                     System.out.print("*");
                 System.out.print("\n");
 
             }else {
 
-                numStart = 2 * i + 1;
-                int numLeftBlack = (totalLines - numStart) / 2;
+                numStar = 2 * i + 1;
+                int numLeftBlack = (totalLines - numStar) / 2;
                 printBlank(numLeftBlack);
-                for (int j = 0; j < numStart;j++)
+                for (int j = 0; j < numStar;j++)
                     System.out.print("*");
                 System.out.print("\n");
             }
